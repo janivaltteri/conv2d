d8b595ca4d87565e10a8acd70d9375d9149d9fc6
Jani Anttila <jva@localhost.localdomain>
2017-04-23 19:07:48 +0300

    initial

---
+++ src/csmf.f95
@@ -0,0 +1,25 @@
+SUBROUTINE csmf(res,mat,ker,mxsize,mysize,oxsize,oysize,ksize,ksizecount)
+  IMPLICIT NONE
+  INTEGER, INTENT(in) :: mxsize,mysize,oxsize,oysize,ksize,ksizecount
+  INTEGER :: i, j, k, l
+  DOUBLE PRECISION, DIMENSION(mxsize,mysize), INTENT(out) :: res
+  DOUBLE PRECISION, DIMENSION(mxsize,mysize), INTENT(in) :: mat
+  DOUBLE PRECISION, DIMENSION(ksize,ksize), INTENT(in) :: ker
+  FORALL (i = 1:mxsize, j = 1:mysize)
+     res(i,j) = 0.0
+  END FORALL
+  !DO i = 1,mxsize
+  !   DO j = 1,mysize
+  !      res(i,j) = 0.0
+  !   END DO
+  !END DO
+  DO i = (1+ksizecount),(oxsize+ksizecount)
+     DO j = (1+ksizecount),(oysize+ksizecount)
+        DO k = -(ksize/2),(ksize/2)
+           DO l = -(ksize/2),(ksize/2)
+              res(i,j) = res(i,j) + ker(k+ksizecount+1,l+ksizecount+1)*mat(i+k,j+l)
+           END DO
+        END DO
+     END DO
+  END DO
+END SUBROUTINE csmf
